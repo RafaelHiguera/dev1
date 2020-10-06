a70b674d04892d05637ebcd7a78a397979177ede
Rafael David Higuera Silva <rafdinho@gmail.com>
2020-10-06 19:49:48 -0300

    Ajout de la creation de fichiers csv, modifiction et finition de la boucle pour trouver fichiers java. Ajout de classe test

--- src/com/umontreal/Main.java
+++ src/com/umontreal/Main.java
@@ -1,11 +1,25 @@
 package com.umontreal;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
-    public int class_LOC(Scanner scanner){
+    public static void main(String[] args) {
+        csvFileGenerator("test");
+    }
+
+
+    public static int class_LOC(Scanner scanner){
         int loc = 0;
         while (scanner.hasNextLine()) {
             String data = scanner.nextLine();
@@ -16,7 +30,7 @@ public class Main {
         return loc;
     }
 
-    public int methode_LOC(Scanner scanner){
+    public static int methode_LOC(Scanner scanner){
         int loc = 0;
         Stack<Character> stack = new Stack();
         stack.push('{');
@@ -41,14 +55,14 @@ public class Main {
     * asd asd
     * sad
     */
-    public int classe_methode_CLOC(Scanner scanner){
+    public static int classe_methode_CLOC(Scanner scanner){
         int loc = 0;
-
         while (scanner.hasNextLine()) {
             String data = scanner.nextLine();
             if(data.contains("//") || (data.contains("/*") && data.contains("*/"))){
                 loc++;
             }else if(data.contains("/*")){
+                loc++;
                 int leftComment = 1;
                 while(leftComment != 0){
                     data = scanner.nextLine();
@@ -65,11 +79,50 @@ public class Main {
         return loc;
     }
 
-    public double classe_dc(Scanner scanner){
+    public static double classe_dc(Scanner scanner){
         return classe_methode_CLOC(scanner) / class_LOC(scanner);
     }
 
-    public double methode_dc(Scanner scanner){
+    public static double methode_dc(Scanner scanner){
         return classe_methode_CLOC(scanner) / methode_LOC(scanner);
+    }
+
+    public static void csvFileGenerator(String path){
+        createCsvFiles("classes");
+        createCsvFiles("methodes");
+        try (Stream<Path> walk = Files.walk(Paths.get(path))) {
+            List<String> result = walk.filter(Files::isRegularFile)
+                    .map(x -> x.toString()).collect(Collectors.toList());
+
+            for(String file : result){
+                if(file.substring(file.length()-4,file.length()).equals("java")){
+                    addClassEntry(file);
+                    addMethodsEntry(file);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createCsvFiles(String fileName) {
+        try {
+            File file = new File(fileName+".csv");
+            file.createNewFile();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    private static void addClassEntry(String file) throws FileNotFoundException {
+        File f = new File(file);
+        Scanner scanner = new Scanner(f);
+        System.out.println(class_LOC(new Scanner(f)));
+        System.out.println(classe_methode_CLOC(scanner));
+    }
+
+    private static void addMethodsEntry(String file) {
+
     }
 }
---
+++ test/New folder/test.java
@@ -0,0 +1,13 @@
+//test
+/*
+ * a sd
+ */
+
+
+
+/* asd */
+class test{
+    public static void main(String args[]){
+        System.out.println("Hello Java");
+    }
+}
